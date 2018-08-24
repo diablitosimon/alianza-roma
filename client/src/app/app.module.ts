bfeb8015a993694ae22052a519ac24c84a5ce596
unknown <diablitosimon@live.com>
2018-08-23 23:40:14 -0500

    Create component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ResetComponent } from './components/password/reset/reset.component';
+import { ResponseComponent } from './components/password/response/response.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    ResetComponent,
+    ResponseComponent
   ],
   imports: [
     BrowserModule
