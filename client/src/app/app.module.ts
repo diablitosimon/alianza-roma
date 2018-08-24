757d427f5eb7e2c632c475eba5bac92ebabaca9a
jpiedrahita <diablitosimon@live.com>
2018-08-24 18:30:02 -0500

    Login component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,32 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { ResetComponent } from './components/password/reset/reset.component';
-import { ResponseComponent } from './components/password/response/response.component';
-import { RoutingModule } from './/routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {LoginComponent} from './components/login/login.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {ResetComponent} from './components/password/reset/reset.component';
+import {ResponseComponent} from './components/password/response/response.component';
+import {RoutingModule} from './/routing.module';
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    LoginComponent,
-    SignupComponent,
-    ProfileComponent,
-    ResetComponent,
-    ResponseComponent
-  ],
-  imports: [
-    BrowserModule,
-    RoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        LoginComponent,
+        SignupComponent,
+        ProfileComponent,
+        ResetComponent,
+        ResponseComponent
+    ],
+    imports: [
+        BrowserModule,
+        RoutingModule,
+        FormsModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    public form = {
+        email: null,
+        password: null
+    };
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
 
-  ngOnInit() {
-  }
+    onSubmit() {
+       return this.http.post('http://localhost:8000/api/auth/login', this.form)
+           .subscribe( data => console.log(data), error => console.log(error));
+    }
+
+    ngOnInit() {
+    }
 
 }
