5bbaf023f0a89c2df268d7541bff168869e79c0f
unknown <diablitosimon@live.com>
2018-08-26 21:55:51 -0500

    Forms

--- client/src/app/components/forms/forms.component.ts
+++ client/src/app/components/forms/forms.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/Auth.service";
+import {TokenService} from "../../services/token.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-forms',
@@ -7,7 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
-  constructor() { }
+  public form = {
+    name: null,
+    email: null,
+    password: null,
+    password_confirmation: null,
+  };
+  public error = [];
+
+  constructor(
+    private service: AuthService,
+    private token: TokenService,
+    private router: Router) {
+  }
+
+  onSubmitRegister() {
+    this.service.signup(this.form)
+      .subscribe(data => this.handleResponse(data),
+        error => this.handleError(error))
+  };
+
+  handleResponse(data) {
+    this.token.handle(data.access_token);
+    this.router.navigateByUrl('profile');
+  }
+
+  handleError(error) {
+    this.error = error.error.errors;
+  }
 
   ngOnInit() {
   }
